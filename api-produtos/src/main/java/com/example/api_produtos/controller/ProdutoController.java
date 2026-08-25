515dc00b2f7ab7e752f1e94820765f5425a8a0f6
Muyuki10 <muyuki2010@gmail.com>
2026-08-25 15:06:11 -0300

    third commit

--- api-produtos/src/main/java/com/example/api_produtos/controller/ProdutoController.java
+++ api-produtos/src/main/java/com/example/api_produtos/controller/ProdutoController.java
@@ -1,4 +1,54 @@
 package com.example.api_produtos.controller;
 
+import com.example.api_produtos.model.Produto;
+import com.example.api_produtos.repository.ProdutoRepository;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+
+@RestController
+@RequestMapping("/produtos")
 public class ProdutoController {
+    private final ProdutoRepository repository;
+
+    public ProdutoController(ProdutoRepository repository) {
+        this.repository = repository;
+    }
+
+    @PostMapping
+    public Produto criar(@RequestBody Produto produto){
+        return repository.save(produto);
+    }
+    @GetMapping
+    public List<Produto> listar(){
+        return repository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public  ResponseEntity<Produto> buscar(@PathVariable Long id){
+        return repository.findById(id).map(ResponseEntity::ok).orElse(ResponseEntity.notFound().build());
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Produto> atualizar(@PathVariable Long id, @RequestBody Produto dados){
+        return  repository.findById(id)
+                .map(produto -> {
+                    produto.setNome(dados.getNome());
+                    produto.setPreco(dados.getPreco());
+                    produto.setAtivo(dados.getAtivo());
+                    produto.setCategoria(dados.getCategoria());
+                    return ResponseEntity.ok(repository.save(produto));
+                })
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deletar(@PathVariable Long id){
+        if (!repository.existsById(id)) {
+            return ResponseEntity.notFound().build();
+        }
+        repository.deleteById(id);
+        return ResponseEntity.noContent().build();
+    }
+
 }
